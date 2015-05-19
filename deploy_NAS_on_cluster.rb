ed2144880e4f68326c086bfcc7a4fd214479c788
Cristian Ruiz <cristian.ruiz@inria.fr>
2015-05-19 13:23:18 +0200

    fixing number of runs

--- deploy_NAS_on_cluster.rb
+++ deploy_NAS_on_cluster.rb
@@ -5,7 +5,6 @@ require 'yaml'
 nodes = []
 SOURCE_NAS =  "http://public.grenoble.grid5000.fr/~cruizsanabria/NPB3.3.tar"
 
-
 puts "Reading machine file"
 
 f = File.open("machine_file", "r")
@@ -14,17 +13,16 @@ f.each_line do |line|
 end
 f.close
 
+
 # making nodes unique
 num_procs = nodes.length
 nodes.uniq!
-
 
 puts "Downloading NAS if does not exist"
 `wget #{SOURCE_NAS} -O /tmp/NAS.tar` unless File.exist?("/tmp/NAS.tar")
 
 
 # Transferring code to the first machine
-
 puts "Sending machine file and code to the first node: #{nodes.first}"
 Net::SCP.start(nodes.first,'root') do |scp|
    puts scp.upload "machine_file", "machine_file"
@@ -33,18 +31,13 @@ end
 
 
 # Compiling on the fist machine
-
 TAU_MAKE = "/usr/local/tau-install/x86_64/lib/Makefile.tau-mpi-pdt"
 
 # Reading the bench to deploy
-
 benchs = YAML.load(File.read("expe_metadata.yaml"))["benchs"]
 
-
-
 NUM_PROCS = ARGV[0]
-RUNS = ARGV[1]
-
+RUNS = ARGV[1].to_i
 
 binaries = []
 puts "compiling NAS bench with with TAU"
