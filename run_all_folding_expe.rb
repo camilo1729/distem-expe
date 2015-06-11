2fa492c98bf645edc36452cdfc1b51e482d82e65
Cristian Ruiz <cristian.ruiz@inria.fr>
2015-06-11 10:26:56 +0200

    Fixing script name in experiment main scripts

--- run_all_folding_expe.rb
+++ run_all_folding_expe.rb
@@ -5,7 +5,7 @@ nodes = [128,64,32,16,8,4]
 lxcpnode = 1
 nodes.each do |num|
 
-  `ruby deploy_kernel_version_expe.rb #{num} #{128/num}`
+  `ruby deploy_lxc_expe.rb #{num} #{128/num}`
   `mkdir nodes_#{num}`
   `mv real_* nodes_#{num}/`
   `mv distem_* nodes_#{num}/`
--- run_all_kernelversion_expe.rb
+++ run_all_kernelversion_expe.rb
@@ -4,7 +4,7 @@ nodes = [64,32,16,8]
 
 nodes.each do |num|
 
-  `ruby deploy_kernel_version_expe.rb #{num}`
+  `ruby deploy_lxc_expe.rb #{num}`
   `mkdir nodes_#{num}`
   `mv real_* nodes_#{num}/`
   `mv distem_* nodes_#{num}/`
