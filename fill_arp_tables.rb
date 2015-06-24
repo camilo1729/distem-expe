90c6ddb2085ed2d38de7a7bc4e8cf32c6a7a6195
Cristian Ruiz <cristian.ruiz@inria.fr>
2015-06-24 10:22:53 +0200

    Fixing ARP script

--- fill_arp_tables.rb
+++ fill_arp_tables.rb
@@ -1,21 +1,28 @@
 require 'cute'
 
-NODE= ARGV[0]
 
 puts "filling the ARP tables"
 
-Net::SSH.start(NODE, 'root') do |ssh|
+nodes = []
+
+f = File.open("machine_file", "r")
+f.each_line do |line|
+  nodes.push(line.chop)
+end
+f.close
+
+Net::SSH.start(nodes.first, 'root') do |ssh|
   ssh.exec!("for i in $(cat machine_file); do ssh $i hostname; done")
   ssh.exec!("arp -n | awk '{ print $1, $3}' | grep ^[0-9] > arpfile")
 end
 
 puts "Downloading ARP file"
-Net::SCP.start(NODE,'root') do |scp|
+Net::SCP.start(nodes.first,'root') do |scp|
     scp.download "arpfile", "arpfile"
 end
 
-log.info "Transferring to all nodes"
-Cute::TakTuk.start(NODE, :user => 'root') do |tak|
+puts "Transferring to all nodes"
+Cute::TakTuk.start(nodes, :user => 'root') do |tak|
   tak.put("arpfile","arpfile")
   tak.exec!("arp -f arpfile")
 end
