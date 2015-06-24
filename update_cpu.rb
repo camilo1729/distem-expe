e09067f6c67991e7b8c09b581ff82244f450ff32
Cristian Ruiz <cristian.ruiz@inria.fr>
2015-06-24 17:49:04 +0200

    Adding update CPU script

---
+++ update_cpu.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+require 'distem'
+
+new_cpu_freq = ARGV[0].to_i
+
+# Updates the values of cpu frequency
+
+Distem.client do |cl|
+
+  info = cl.vnodes_info
+
+  info.each do |vnode|
+    cl.vcpu_update(vnode["name"], new_cpu_freq, 'ratio')
+
+  end
+
+end
