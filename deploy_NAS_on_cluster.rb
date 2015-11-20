f3d03393694337685dc0cb6a11a696846790c2b0
Cristian Ruiz <cristian.ruiz@inria.fr>
2015-11-20 11:38:56 +0100

    Got rid of timeout for mpi

--- deploy_NAS_on_cluster.rb
+++ deploy_NAS_on_cluster.rb
@@ -106,7 +106,6 @@ Net::SSH.start(nodes.first, 'root') do |ssh|
       log.info "Executing binary: #{binary}"
       mpi_cmd = "mpirun  --mca btl self,sm,tcp --machinefile machine_file #{binary}"
       mpi_cmd = "mpirun  --allow-run-as-root --mca btl self,sm,tcp --machinefile machine_file #{binary}" if metadata["mpi_version"] == "1.8.5"
-      mpi_cmd = "timeout #{metadata["timeout"]} " + mpi_cmd if metadata["timeout"]
       ssh.exec!(mpi_cmd)
       log.info "Getting the profile files"
       profile_dir = "profile-#{binary}-#{Time.now.to_i}"
